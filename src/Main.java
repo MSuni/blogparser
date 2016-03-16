648cd1a63b06dd1c3e19a9eab647b549d1c715f9
MSuni <mikko.suni@hotmail.com>
2016-03-16 14:22:40 +0200

    Change execution code

--- src/Main.java
+++ src/Main.java
@@ -18,46 +18,37 @@ public class Main {
 
   public static void main(String[] args) {
 
-    
     Facer facer = new Facer();
     facer.changeFile("face4.txt");
     facer.setpage("https://www.facebook.com/BurgerKingFI/reviews");
-//    facer.setpage("file:///C:/Users/Blackstorm/Desktop/shen/vabootedu.htm");
+    // facer.setpage("file:///C:/Users/Blackstorm/Desktop/shen/vabootedu.htm");
     facer.login("eerotuomisto@luukku.com", "testisalasana");
     System.out.println("getting user links");
     ArrayList<String> users = facer.getUsers();
-//    ArrayList<String> users = new ArrayList<String>();
-//    users.add("https://www.facebook.com/profile.php?id=100009909642941");
-    
+    // ArrayList<String> users = new ArrayList<String>();
+    // users.add("https://www.facebook.com/profile.php?id=100009909642941");
+
     System.out.println("going to scrape user info");
     System.out.println(": " + users.size());
-    for (int i = 1; i < 6 ; i++){
-      if (users.get(i).equals("https://www.facebook.com/BurgerKingFI/")){
+    for (int i = 6; i < 15; i++) {
+      if (users.get(i).equals("https://www.facebook.com/BurgerKingFI/")) {
 	continue;
       }
-      
-      if (users.get(i).substring(25, 32).equals("profile")){
+
+      System.out.println("here: " + users.get(i));
+      if (users.get(i).substring(25, 32).equals("profile")) {
 	facer.getUserInfo(users.get(i), 2);
       } else {
 	facer.getUserInfo(users.get(i), 1);
       }
-      System.out.println("here: " + users.get(i));
     }
-//    for (int i = 0; i < 3; i++){
-//      System.out.println(users.get(i).substring(25, 32));
-//      if (users.get(i).substring(25, 32).equals("profile")){
-//	facer.getUserInfo(users.get(i), 2);
-//      } else {
-//	facer.getUserInfo(users.get(i), 1);
-//      }
-//    }
-//    facer.nodeTest();
-    
-    
-    
-//    Cumulus cumulus = new Cumulus();
-//    cumulus.usehtmlunit(4, 4, "newWave2");
-//    cumulus.getOneHotel("http://www.tripadvisor.com/Hotel_Review-g189942-d249056-Reviews-Cumulus_Jyvaskyla_Hotel-Jyvaskyla_Central_Finland.html", "thisworks");
+
+    // testWriter();
+
+    // Cumulus cumulus = new Cumulus();
+    // cumulus.usehtmlunit(2, 2, "stars");
+    // cumulus.getOneHotel("http://www.tripadvisor.com/Hotel_Review-g189942-d249056-Reviews-Cumulus_Jyvaskyla_Hotel-Jyvaskyla_Central_Finland.html",
+    // "thisworks");
   }
 
   private static void testWriter() {
@@ -88,57 +79,74 @@ public class Main {
 
     try {
 
-      HtmlPage page = webClient.getPage(
-	  "http://www.tripadvisor.com/ShowUserReviews-g189948-d238534-r352042025-Hotelli_Cumulus_Koskikatu-Tampere_Pirkanmaa.html#CHECK_RATES_CONT");
+      HtmlPage page = webClient.getPage("https://www.tripadvisor.fi/ShowUserReviews-g189908-d249055-r320851008-Cumulus_Kuopio_Hotel-Kuopio_Northern_Savonia.html#CHECK_RATES_CONT");
       webClient.waitForBackgroundJavaScript(1 * 1000);
-      
-//       List<DomAttr> rating2 = (List) page.getByXPath("//img[@class='sprite-rating_no_fill rating_no_fill no30']/@alt");
 
-//       rating2.get(0);
-//       bw.write(rating2.get(0).toString());
+      List<DomAttr> rating1 = (List) page.getByXPath("//img[@class='sprite-rating_s_fill rating_s_fill s10' or @class='sprite-rating_s_fill rating_s_fill s20' or @class='sprite-rating_s_fill rating_s_fill s30' or @class='sprite-rating_s_fill rating_s_fill s40' or @class='sprite-rating_s_fill rating_s_fill s50']/@alt");
+      System.out.println("Rating is " + rating1.get(0).getValue());
+      switch (rating1.get(0).getValue()) {
+      case "1/5 tähteä":
+	System.out.println("case 1");
+	break;
+      case "2/5 tähteä":
+	System.out.println("case 2");
+	break;
+      case "3/5 tähteä":
+	System.out.println("case 3");
+	break;
+      case "4/5 tähteä":
+	System.out.println("case 4");
+	break;
+      case "5/5 tähteä":
+	System.out.println("case 5");
+	break;
+      }
 
+      // rating2.get(0);
+      // bw.write(rating2.get(0).toString());
 
-	 System.out.println("Anything here?");
+      System.out.println("Anything here?");
 
-//	  List<DomElement> pageLinks = (List) page.getByXPath("//div");
-       List<DomElement> divs = (List) page.getByXPath("//div[@class='entry']");
-//       List<DomAttr> attris = (List) page.getByXPath("//div[@class='entry']/@p");
-//	 System.out.println("list size " + pageLinks.size());
-//	 System.out.println("list size " + attris.size());
+      // List<DomElement> pageLinks = (List) page.getByXPath("//div");
+      List<DomElement> divs = (List) page.getByXPath("//div[@class='entry']");
+      // List<DomAttr> attris = (List)
+      // page.getByXPath("//div[@class='entry']/@p");
+      // System.out.println("list size " + pageLinks.size());
+      // System.out.println("list size " + attris.size());
 
-//	 bw.write(page.asText());
-//	 bw.write(page.getWebResponse().getContentAsString());
-       for (int i = 0; i < divs.size(); i++){
-	 System.out.println("found something");
-//	 System.out.println("it's here " + pageLinks.get(i).getTextContent());
-//	 bw.write(page.asText());
-//	 bw.write(pageLinks.get(i).getTextContent() + System.getProperty("line.separator"));
-//	 bw.write(divs.get(i) + System.getProperty("line.separator"));
-//	 bw.write(divs.get(i).asText() + System.getProperty("line.separator"));
-	 bw.write(divs.get(i).getTextContent() + System.getProperty("line.separator"));
-       }
+      // bw.write(page.asText());
+      // bw.write(page.getWebResponse().getContentAsString());
+      for (int i = 0; i < divs.size(); i++) {
+	System.out.println("found something");
+	// System.out.println("it's here " + pageLinks.get(i).getTextContent());
+	// bw.write(page.asText());
+	// bw.write(pageLinks.get(i).getTextContent() +
+	// System.getProperty("line.separator"));
+	// bw.write(divs.get(i) + System.getProperty("line.separator"));
+	// bw.write(divs.get(i).asText() +
+	// System.getProperty("line.separator"));
+	bw.write(divs.get(i).getTextContent() + System.getProperty("line.separator"));
+      }
 
-       
-//       List<DomElement> textDiv = (List) page.getByXPath("//div[@class='entry']");
-//       textDiv.get(0);
-//       for (int i = 0; i < textDiv.size(); i++){
-//
-//	    System.out.println("haloo " + textDiv.get(i).getTextContent());
-//	 
-//       }
-       
-//	if (!textDiv.isEmpty()) {
-//	    System.out.println("got the text");
-//	    System.out.println(textDiv.get(0).getTextContent());
-//
-//	    bw.write(textDiv.get(0).getTextContent() + System.getProperty("line.separator"));
-//	} else {
-//	    System.out.println("no text found");
-//	}
-      
-      
-      
-      
+      // List<DomElement> textDiv = (List)
+      // page.getByXPath("//div[@class='entry']");
+      // textDiv.get(0);
+      // for (int i = 0; i < textDiv.size(); i++){
+      //
+      // System.out.println("haloo " + textDiv.get(i).getTextContent());
+      //
+      // }
+
+      // if (!textDiv.isEmpty()) {
+      // System.out.println("got the text");
+      // System.out.println(textDiv.get(0).getTextContent());
+      //
+      // bw.write(textDiv.get(0).getTextContent() +
+      // System.getProperty("line.separator"));
+      // } else {
+      // System.out.println("no text found");
+      // }
+
     } catch (FailingHttpStatusCodeException e1) {
       e1.printStackTrace();
     } catch (MalformedURLException e1) {
