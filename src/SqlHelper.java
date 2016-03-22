30b86d9bd3276b1db6a6263b813edb692d27e944
MSuni <mikko.suni@hotmail.com>
2016-03-22 14:33:16 +0200

    Move query code to getUserID(), add disconnect()

--- src/SqlHelper.java
+++ src/SqlHelper.java
@@ -9,44 +9,61 @@ import com.mysql.jdbc.Statement;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class SqlHelper {
-  
+
   String user;
   String password;
   String server;
+  String databaseName;
   MysqlDataSource dataSource;
   Connection connection;
-  
-  public SqlHelper(String user, String password, String server){
-    
+
+  public SqlHelper(String user, String password, String server, String databaseName) {
+
     this.user = user;
     this.password = password;
     this.server = server;
-    
+    this.databaseName = databaseName;
+
     dataSource = new MysqlDataSource();
     dataSource.setUser(user);
     dataSource.setPassword(password);
     dataSource.setServerName(server);
-    dataSource.setDatabaseName("FBminer2");
-    
-    
+    dataSource.setDatabaseName(databaseName);
+
   }
-  
-  public void connect(){
+
+  public void connect() {
     try {
       connection = dataSource.getConnection();
       System.out.println("Connection Succesful ");
-      
-      Statement state = (Statement) connection.createStatement();
 
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  public void disconnect(){
+      try {
+	connection.close();
+      System.out.println("Connection Closed");
+      } catch (SQLException e) {
+	  System.out.println("Exception on disconnect");
+	e.printStackTrace();
+      }
+  }
+
+  public void getUserID(int id) {
+
+    Statement state;
+    try {
+      state = (Statement) connection.createStatement();
       System.out.println("getting info...");
-      ResultSet result = state.executeQuery("SELECT user_name, user_id, app01_id FROM UserRegistry WHERE app01_id IS NOT NULL LIMIT 10 ");
+      ResultSet result = state.executeQuery("SELECT user_name, user_id, app01_id FROM UserRegistry WHERE app01_id IS NOT NULL AND id = " + id  + " LIMIT 10 ");
       System.out.println("result acquired");
       result.next();
       System.out.println("result: " + result.getString(1) + " " + result.getString(2) + " " + result.getString(3));
       result.close();
       state.close();
-      connection.close();
-      
     } catch (SQLException e) {
       e.printStackTrace();
     }
